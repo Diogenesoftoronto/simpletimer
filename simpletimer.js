a4bda9d4cc5b9fb8277f28b3e7107d4246bb52f6
diogenesoftoronto <diogenesoft@protonmail.com>
2021-11-30 20:52:04 -0500

    finsihed boy

--- simpletimer.js
+++ simpletimer.js
@@ -1,5 +1,11 @@
 'use strict';
 const args = process.argv.slice(2); //array of args
 args.forEach( arg => {
+  if (arg < 0) {
+    continue;
+  }
+  if (arg === NaN) {
+    continue;
+  }
   setTimeout(() => process.stdout.write('.'), arg);
 })
